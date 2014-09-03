1253fb744aa8282afc97eecc1f991a3ba9912a76
Felipe Freichos Godoy <ffelipegodoy@gmail.com>
2014-09-02 23:56:44 -0300

    Teste de física de dado

---
+++ Code/Assets/Scripts/AngularSpeedStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngularSpeedStart : MonoBehaviour {
+
+	public Vector3 angularSpeed = new Vector3(-40,20,40);
+	void Start () {
+		rigidbody.maxAngularVelocity = 100f;
+		rigidbody.AddTorque(angularSpeed,ForceMode.VelocityChange);
+	}
+}
