21cb6906bd5ff2c02fb075cf4fb72036bd1ba534
Felipe Freichos Godoy <ffelipegodoy@gmail.com>
2014-09-04 18:37:36 -0300

    Imagens dos territórios adicionadas.

---
+++ Code/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour {
+
+	public float offset = -1.14f;
+	public float magnitude = 6f;
+	public float speed = 0.5f;
+	public Vector3 accelerometer;
+	public float avr = 0f;
+	private float lastAvr = 0f;
+
+	public float velocity = 0f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		accelerometer = Input.acceleration;
+//		avr = accelerometer.sqrMagnitude;
+		avr = Mathf.Round((accelerometer.sqrMagnitude - offset) * magnitude) / magnitude;
+//		transform.Translate(0f,avr * speed, 0f);
+		velocity += (((lastAvr + avr) / 2f) * Time.deltaTime) - velocity * 0.1f;
+		transform.Translate(0f,velocity * Time.deltaTime, 0f);
+		lastAvr = avr;
+	}
+}
---
+++ Code/Assets/Scripts/DiceSideInfo.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class DiceSideInfo : MonoBehaviour {
+
+	public int diceNumber = -1;
+
+	// Update is called once per frame
+	void Update () {
+		diceNumber = DiceSide(transform.rotation.eulerAngles);
+	}
+
+	public int DiceSide(Vector3 eulerAngles){
+		eulerAngles.x = Mathf.Round(eulerAngles.x / 90f) % 4f;
+		eulerAngles.y = Mathf.Round(eulerAngles.y / 90f) % 4f;
+		eulerAngles.z = Mathf.Round(eulerAngles.z / 90f) % 4f;
+		if(eulerAngles.x == 1f)
+			return 2;
+		if(eulerAngles.x == 3f)
+			return 5;
+		if(eulerAngles.x ==0f){
+			if(eulerAngles.z == 0f)
+				return 1;
+			if(eulerAngles.z == 1f)
+				return 3;
+			if(eulerAngles.z == 2f)
+				return 6;
+			else
+				return 4;
+		}
+		if(eulerAngles.x == 2f){
+			if(eulerAngles.z == 0f)
+				return 6;
+			if(eulerAngles.z == 1f)
+				return 4;
+			if(eulerAngles.z == 2f)
+				return 1;
+			else
+				return 3;
+		}
+		return -1;
+	}
+}
