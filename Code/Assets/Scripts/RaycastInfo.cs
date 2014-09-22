f2cf7886c8b0fce9943ddca20ecef95e9147fafb
Felipe Freichos Godoy <ffelipegodoy@gmail.com>
2014-09-22 15:03:55 -0300

    Testando vizinhos. Agora ao rodar a cena, pode-se notar os vizinhos.

--- Code/Assets/Scripts/RaycastInfo.cs
+++ Code/Assets/Scripts/RaycastInfo.cs
@@ -6,6 +6,7 @@ public class RaycastInfo : MonoBehaviour {
 	public float animationTime = 0.5f;
 	public iTween.EaseType ease = iTween.EaseType.easeInBack;
 	public Color animationColor = new Color(0.9f,0.9f,0.9f);
+	public Color animationNeibColor = new Color(0.1f,0.1f,0.1f);
 	public GameObject troopPrefab;
 	public Vector3 troopOffset = new Vector3(0,0,-1);
 	
@@ -18,17 +19,14 @@ public class RaycastInfo : MonoBehaviour {
 			Vector2 point = new Vector2(raycast.point.x, raycast.point.y);
 			Collider2D hitCollider = Physics2D.OverlapPoint(point);
 			if(hitCollider){
-				TerritoryInfo territory = hitCollider.GetComponentInChildren<TerritoryInfo>();
-				if(territory != null){
-					iTween.ValueTo(territory.gameObject,iTween.Hash(iT.ValueTo.to, territory.color, 
-					                                                iT.ValueTo.from, this.animationColor, 
-					                                                iT.ValueTo.onupdatetarget, territory.gameObject,
-					                                                iT.ValueTo.onupdate, "SetAnimationColor",
-					               									iT.ValueTo.time, this.animationTime,
-					               									iT.ValueTo.easetype, ease));
+				TerritoryInfo territoryInfo = hitCollider.GetComponentInChildren<TerritoryInfo>();
+				Territory territory = hitCollider.GetComponentInChildren<Territory>();
+				if(territoryInfo != null){
+					SetColorTerritoryInfo(territoryInfo);
+					SetColorNeighbors(territory);
 					if(Input.GetMouseButtonDown(0)){
 						GameObject g = (GameObject)Instantiate(troopPrefab);
-						g.transform.position = territory.transform.position;
+						g.transform.position = territoryInfo.transform.position;
 						g.transform.Translate(troopOffset);
 						AngularSpeedStart a = g.AddComponent<AngularSpeedStart>();
 						a.angularSpeed.x = Random.Range(-18f,18f);
@@ -36,6 +34,29 @@ public class RaycastInfo : MonoBehaviour {
 						a.angularSpeed.z = Random.Range(-18f,18f);
 					}
 				}
+			}
+		}
+	}
+
+	void SetColorTerritoryInfo(TerritoryInfo territoryInfo){
+		iTween.ValueTo(territoryInfo.gameObject,iTween.Hash(iT.ValueTo.to, territoryInfo.color, 
+		                                                    iT.ValueTo.from, this.animationColor, 
+		                                                    iT.ValueTo.onupdatetarget, territoryInfo.gameObject,
+		                                                    iT.ValueTo.onupdate, "SetAnimationColor",
+		                                                    iT.ValueTo.time, this.animationTime,
+		                                                    iT.ValueTo.easetype, ease));
+	}
+
+	void SetColorNeighbors(Territory territory){
+		if(territory != null && territory.neighbors != null){
+			foreach(Territory neigbor in territory.neighbors){
+				TerritoryInfo territoryInfo = neigbor.GetComponent<TerritoryInfo>();
+				iTween.ValueTo(territoryInfo.gameObject,iTween.Hash(iT.ValueTo.to, territoryInfo.color, 
+				                                                    iT.ValueTo.from, this.animationNeibColor, 
+				                                                    iT.ValueTo.onupdatetarget, territoryInfo.gameObject,
+				                                                    iT.ValueTo.onupdate, "SetAnimationColor",
+				                                                    iT.ValueTo.time, this.animationTime,
+				                                                    iT.ValueTo.easetype, ease));
 			}
 		}
 	}
