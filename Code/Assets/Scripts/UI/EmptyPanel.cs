cfcbfbffb021ebfab20e222e90c3b3a01fd007b5
Vinícius Henriques <vinicius1991oh@gmail.com>
2014-10-18 15:21:32 -0300

    Formato para telas e Lobby clicável
    Modelar mais!

---
+++ Code/Assets/Scripts/UI/EmptyPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyPanel : MonoBehaviour {
+	public IaPanel iaPanel;
+	public PlayerPanel playerPanel;
+	// Use this for initialization
+
+	public void setIA(){
+		IaPanel p = (IaPanel)Instantiate (iaPanel);
+		p.transform.position = transform.position;	//Deveria ser Rect Transform! To sem internet pra resolver isso.
+		p.transform.parent = this.transform.parent.transform;
+		Destroy (this.gameObject);
+	}
+
+	public void setPlayer(){
+		PlayerPanel p = (PlayerPanel)Instantiate (playerPanel);
+		p.transform.position = transform.position;	//Deveria ser Rect Transform! To sem internet pra resolver isso.
+		p.transform.parent = this.transform.parent.transform;
+		Destroy (this.gameObject);;
+	}
+
+}
---
+++ Code/Assets/Scripts/UI/IaPanel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IaPanel : MonoBehaviour {
+	public EmptyPanel emptyPanel;
+
+	public void setEmpty(){
+		EmptyPanel p = (EmptyPanel)Instantiate (emptyPanel);
+		p.transform.position = transform.position;	//Deveria ser Rect Transform! To sem internet pra resolver isso.
+		p.transform.parent = this.transform.parent.transform;
+		Destroy (this.gameObject);
+	}
+}
