a8276642e8e24ee7bd412f5fa44fd37639ceff2d
Vinícius Henriques <vinicius1991oh@gmail.com>
2014-11-02 20:24:15 -0200

    GUI In-game
    Luz de estados e funções de troca prontas.
    Falta interligar.

---
+++ Code/Assets/Scripts/UI/In-Game/StatesHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatesHandler : MonoBehaviour {
+	public string alocar;
+	public string atacar;
+	public string mover;
+
+	public void setAlocar(){
+		ChangeColor[] t;
+		t = GetComponentsInChildren<ChangeColor> ();
+		foreach (ChangeColor c in t) {
+			if(c.gameObject.name == "Light1"){
+				c.activate();
+				}
+			else{
+				c.deactivate();
+			}
+		}
+		Text txt = this.GetComponentInChildren<Text> ();
+		txt.text = alocar;
+	}
+
+	public void setAtacar(){
+		ChangeColor[] t;
+		t = GetComponentsInChildren<ChangeColor> ();
+		foreach (ChangeColor c in t) {
+			if(c.gameObject.name == "Light2"){
+				c.activate();
+			}
+			else{
+				c.deactivate();
+			}
+		}
+		Text txt = this.GetComponentInChildren<Text> ();
+		txt.text = atacar;
+	}
+
+	public void setMover(){
+		ChangeColor[] t;
+		t = GetComponentsInChildren<ChangeColor> ();
+		foreach (ChangeColor c in t) {
+			if(c.gameObject.name == "Light3"){
+				c.activate();
+			}
+			else{
+				c.deactivate();
+			}
+		}
+		Text txt = this.GetComponentInChildren<Text> ();
+		txt.text = mover;
+	}
+}
