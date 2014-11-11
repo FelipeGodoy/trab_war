1a59c7629bc91e02ace78b4691547c49f688ab0b
Felipe Freichos Godoy <ffelipegodoy@gmail.com>
2014-11-11 17:39:06 -0200

    implementação do dado viciado

--- Code/Assets/Scripts/DiceSideInfo.cs
+++ Code/Assets/Scripts/DiceSideInfo.cs
@@ -3,12 +3,46 @@ using System.Collections;
 [ExecuteInEditMode]
 public class DiceSideInfo : MonoBehaviour {
 
+
+
+	private const float MIN_SPEED_TO_CHANGE = 0.3f;
+
 	public int diceNumber = -1;
+	public int forcedNumber = -1;
+	public float impulse = 10f;
+
+	protected Vector3 angularVelocity;
+	protected Vector3 lastAngularVelocity = new Vector3(0f,0f,0f);
+	protected int dirFlag = 0;
 
 	private readonly Vector3 defaultAngle = new Vector3(90f,0f,0f);
 
 	void Update () {
-		diceNumber = DiceSide(Quaternion.Inverse(Quaternion.Euler(Camera.main.transform.rotation.eulerAngles - defaultAngle)) * transform.rotation);
+		diceNumber = DiceSideByPerspective(transform.rotation);
+	}
+
+	void FixedUpdate(){
+		if(forcedNumber == Mathf.Clamp(forcedNumber,1,6)){
+			angularVelocity = rigidbody.angularVelocity;
+			if(angularVelocity.magnitude <= MIN_SPEED_TO_CHANGE && diceNumber != forcedNumber){
+				switch(dirFlag){
+				case 0:{
+					rigidbody.AddTorque(new Vector3(impulse,0f,0f),ForceMode.Impulse);
+					dirFlag = 1;
+					break;
+				}
+				case 1:{
+					rigidbody.AddTorque(new Vector3(0f,impulse,0f),ForceMode.Impulse);
+					dirFlag = 0;
+					break;
+				}
+				}
+			}
+		}
+	}
+
+	public int DiceSideByPerspective(Quaternion angle){
+		return DiceSide(Quaternion.Inverse(Quaternion.Euler(Camera.main.transform.rotation.eulerAngles - defaultAngle)) * angle);
 	}
 
 	public int DiceSide(Quaternion angle){
