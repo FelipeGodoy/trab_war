00574ca10cdb8720324158c34ba68e893aee87ed
Vinícius Henriques <vinicius1991oh@gmail.com>
2014-11-12 19:41:04 -0200

    Alert.Message ==> Message.New :email:

---
+++ Code/Assets/Scripts/UI/In-Game/Message.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+	public GameObject alert;
+	static Message example;
+
+	void Start(){
+		example = this;
+	}
+
+	public void position(Alert obj){
+		obj.transform.position = this.transform.position;
+		obj.transform.parent = this.transform;
+		RectTransform r = obj.GetComponent<RectTransform>();
+		RectTransform model = this.GetComponent<RectTransform>();
+		r.sizeDelta = model.sizeDelta;
+	}
+
+	public static void New(string title, string message, Alert.Delegate onClose){
+		GameObject g = (GameObject)Resources.Load("Alert");
+		//Alert a = g.GetComponent<Alert>();
+		Alert a = ((GameObject)Instantiate (g)).GetComponent<Alert>();
+		a.set(title,message);
+		a.onClose = onClose;
+		example.position (a);
+	}
+
+	public void test(){
+		Message.New ("HUEHU", "heuheuheuheuheuheu", null);
+	}
+}
--- Code/Assets/Scripts/request/RequestController.cs
+++ Code/Assets/Scripts/request/RequestController.cs
@@ -138,7 +138,7 @@ public class RequestController : MonoBehaviour {
 		yield return www;
 		if(www.error != null){
 			Debug.LogError("Request Error: "+www.error);
-			Alert.Message("Error",www.error,delegate {
+			Message.New("Error",www.error,delegate {
 				Application.LoadLevel("Menu");
 		});
 		}
